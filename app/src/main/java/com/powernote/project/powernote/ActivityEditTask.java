4313148ed0bc861b89c560e9d7d9510b2b6d795d
maksymvasylenko <vasylenkomaksymukr@gmail.com>
2017-09-11 14:57:27 +0200

    added ActivityEditTask

---
+++ app/src/main/java/com/powernote/project/powernote/ActivityEditTask.java
@@ -0,0 +1,26 @@
+package com.powernote.project.powernote;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Maks on 11.09.2017.
+ */
+
+public class ActivityEditTask  extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_edit);
+
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/powernote/project/powernote/MainActivity.java
+++ app/src/main/java/com/powernote/project/powernote/MainActivity.java
@@ -1,5 +1,6 @@
 package com.powernote.project.powernote;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -95,8 +96,12 @@ public class MainActivity extends AppCompatActivity{
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent myIntent = new Intent(MainActivity.this, ActivityEditTask.class);
+                startActivity(myIntent);
+
+
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
 
